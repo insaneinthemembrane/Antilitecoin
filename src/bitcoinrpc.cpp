755065874eff88d9a7769189399f74dbf91fbe7b
Anthony Coind <antilitecoin@linuxmail.org>
2017-03-16 10:10:08 +0000

    .

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -39,7 +39,7 @@ static boost::thread_group* rpc_worker_group = NULL;
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 18795 : 8795;
+    return GetBoolArg("-testnet", false) ? 18794 : 8794;
 }
 
 Object JSONRPCError(int code, const string& message)
